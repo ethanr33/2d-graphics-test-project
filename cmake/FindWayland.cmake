c36ab4c5ac7c64ea2a539016c32f16497daa15d0
Ethan Roos <eroos001@ucr.edu>
2026-04-18 17:37:16 -0700

    Updated FindWayland to be more robust

--- cmake/FindWayland.cmake
+++ cmake/FindWayland.cmake
@@ -1,36 +1,31 @@
+find_path(
+  WAYLAND_CLIENT_INCLUDE_DIR
+  NAMES wayland-client.h
+)
 
-# - Try to find Wayland.
-# Once done, this will define
-#
-#  WAYLAND_FOUND - system has Wayland.
-#  WAYLAND_INCLUDE_DIRS - the Wayland include directories
-#  WAYLAND_LIBRARIES - link these to use Wayland.
-#
-# Copyright (C) 2014 Igalia S.L.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-# 1.  Redistributions of source code must retain the above copyright
-#     notice, this list of conditions and the following disclaimer.
-# 2.  Redistributions in binary form must reproduce the above copyright
-#     notice, this list of conditions and the following disclaimer in the
-#     documentation and/or other materials provided with the distribution.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND ITS CONTRIBUTORS ``AS
-# IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR ITS
-# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
-# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+find_library(
+  WAYLAND_CLIENT_LIBRARY
+  NAMES wayland-client libwayland-client libwayland-client0
+)
 
-find_package(PkgConfig)
-pkg_check_modules(WAYLAND wayland-client wayland-server)
+if(WAYLAND_CLIENT_INCLUDE_DIR AND WAYLAND_CLIENT_LIBRARY)
+  add_library(wayland::client UNKNOWN IMPORTED)
+
+  set_target_properties(
+    wayland::client PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${WAYLAND_CLIENT_INCLUDE_DIR}"
+    IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+    IMPORTED_LOCATION "${WAYLAND_CLIENT_LIBRARY}"
+  )
+endif()
 
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Wayland DEFAULT_MSG WAYLAND_LIBRARIES)
+find_package_handle_standard_args(
+  Wayland
+  REQUIRED_VARS WAYLAND_CLIENT_LIBRARY WAYLAND_CLIENT_INCLUDE_DIR
+)
+
+mark_as_advanced(
+  WAYLAND_CLIENT_INCLUDE_DIR
+  WAYLAND_CLIENT_LIBRARY
+)
