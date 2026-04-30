1a982a310949e88f1c07981ce26963acf047b476
Ethan Roos <eroos001@ucr.edu>
2026-04-30 15:28:57 -0700

    Finished adding multiplication to Matrix class

--- include/utils/Matrix.h
+++ include/utils/Matrix.h
@@ -1,13 +1,14 @@
 #pragma once
 
 #include <vector>
+#include <iostream>
 
 class Matrix {
     private:
         int rows;
         int cols;
 
-        std::vector<double> matrix;
+        std::vector<std::vector<double>> matrix;
 
         // Handle case where matrix is not square
         /**
@@ -15,7 +16,7 @@ class Matrix {
          */
         void set_identity();
     public:
-        Matrix(int rows, int cols, bool identity=false) : rows(rows), cols(cols), matrix(rows * cols, 0) {
+        Matrix(int rows, int cols, bool identity=false) : rows(rows), cols(cols), matrix(rows, std::vector<double>(cols, 0)) {
             if (identity) {
                 set_identity();
             }
@@ -33,14 +34,14 @@ class Matrix {
          * @brief Sets element at position (row, col) to val
          */
         inline void set_element(int row, int col, double val) {
-            this->matrix.at(row * rows + col) = val;
+            this->matrix.at(row).at(col) = val;
         }
 
         /**
          * @brief Gets element at (row, col) position
          */
         inline double get_element(int row, int col) const {
-            return this->matrix.at(rows * row + col);
+            return this->matrix.at(row).at(col);
         }
 
         Matrix& operator+=(const Matrix& rhs) {
@@ -55,6 +56,34 @@ class Matrix {
 
         const Matrix operator+(const Matrix& rhs) const {
             return Matrix(*this) += rhs;
+        }
+
+        const Matrix& operator*(const Matrix& rhs) {
+            int m = this->rows;
+            int n = this->cols;
+            int k = rhs.cols;
+
+            Matrix* M = new Matrix(m, k);
+
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < k; j++) {
+                    double sum = 0;
+
+                    for (int l = 0; l < n; l++) {
+                        sum += this->get_element(i, l) * rhs.get_element(l, j);
+                    }
+
+                    M->set_element(i, j, sum);
+                }
+            }
+
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < k; j++) {
+                    this->set_element(i, j, M->get_element(i, j));
+                }
+            }
+
+            return *M;
         }
 
 };
--- test/MatrixTest.cpp
+++ test/MatrixTest.cpp
@@ -63,6 +63,60 @@ TEST_CASE("Check += operator") {
 }
 
 TEST_CASE("Check * operator") {
+    
+    SUBCASE("Multiplying 1x1 matrices") {
+        Matrix m1(1, 1);
+        m1.set_element(0, 0, 2);
+
+        Matrix m2(1, 1);
+        m2.set_element(0, 0, 3);
+
+        Matrix m3 = m1 * m2;
+
+        CHECK(m3.get_rows() == 1);
+        CHECK(m3.get_cols() == 1);
+        CHECK(m3.get_element(0, 0) == 6);
+    }
+
+    SUBCASE("Multiplying 2x2 matrices") {
+        Matrix m1(2, 2);
+        m1.set_element(0, 0, 2);
+        m1.set_element(0, 1, 0);
+        m1.set_element(1, 0, 4);
+        m1.set_element(1, 1, 5);
+
+        Matrix m2(2, 2);
+        m2.set_element(0, 0, 3);
+        m2.set_element(1, 1, -1);
+
+        Matrix m3 = m1 * m2;
+
+        CHECK(m3.get_rows() == 2);
+        CHECK(m3.get_cols() == 2);
+        CHECK(m3.get_element(0, 0) == 6);
+        CHECK(m3.get_element(0, 1) == 0);
+        CHECK(m3.get_element(1, 0) == 12);
+        CHECK(m3.get_element(1, 1) == -5);
+    }
+
+    SUBCASE("Multiplying 2x2 and 2x1 matrices") {
+        Matrix m1(2, 2);
+        m1.set_element(0, 0, 2);
+        m1.set_element(0, 1, 0);
+        m1.set_element(1, 0, 4);
+        m1.set_element(1, 1, 5);
+
+        Matrix m2(2, 1);
+        m2.set_element(0, 0, 3);
+        m2.set_element(1, 0, -1);
+
+        Matrix m3 = m1 * m2;
+
+        CHECK(m3.get_rows() == 2);
+        CHECK(m3.get_cols() == 1);
+        CHECK(m3.get_element(0, 0) == 6);
+        CHECK(m3.get_element(1, 0) == 7);
+    }
 
 }
 
