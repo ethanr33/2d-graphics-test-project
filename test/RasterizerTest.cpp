9830aa879f68056f0e21d4dec7769ce2f24aaafd
Ethan Roos <eroos001@ucr.edu>
2026-05-16 12:04:36 -0700

    Added test cases for vertical lines

--- test/RasterizerTest.cpp
+++ test/RasterizerTest.cpp
@@ -54,6 +54,109 @@ TEST_CASE("Rasterize line") {
         }
     }
 
+    SUBCASE("Vertical line") {
+        Primitive p(PRIMITIVE_TYPE::LINE, Vertex(0, 0), Vertex(0, 5));
+        p.color = Color(255, 0, 0);
+
+
+        commands.push_back(AddPrimitiveCommand(p));
+
+        r.make_fragments(commands);
+        r.render_fragments(f);
+
+        std::vector<Color> expected(100);
+        expected.at(0) = red;
+        expected.at(10) = red;
+        expected.at(20) = red;
+        expected.at(30) = red;
+        expected.at(40) = red;
+        expected.at(50) = red;
+
+        for (int i = 0; i < 100; i++) {
+            INFO("Check failed at ", i, " Expected: ", expected.at(i).to_hex(), " Found: ", f.get_frame_buffer().at(i).to_hex());
+            CHECK(f.get_frame_buffer().at(i) == expected.at(i));
+        }
+    }
+
+    SUBCASE("Vertical line with top clipping") {
+        Primitive p(PRIMITIVE_TYPE::LINE, Vertex(0, -4), Vertex(0, 3));
+        p.color = Color(255, 0, 0);
+
+
+        commands.push_back(AddPrimitiveCommand(p));
+
+        r.make_fragments(commands);
+        r.render_fragments(f);
+
+        std::vector<Color> expected(100);
+        expected.at(0) = red;
+        expected.at(10) = red;
+        expected.at(20) = red;
+        expected.at(30) = red;
+
+        for (int i = 0; i < 100; i++) {
+            INFO("Check failed at ", i, " Expected: ", expected.at(i).to_hex(), " Found: ", f.get_frame_buffer().at(i).to_hex());
+            CHECK(f.get_frame_buffer().at(i) == expected.at(i));
+        }
+    }
+
+    SUBCASE("Vertical line with bottom clipping") {
+        Primitive p(PRIMITIVE_TYPE::LINE, Vertex(0, 1), Vertex(0, 30));
+        p.color = Color(255, 0, 0);
+
+
+        commands.push_back(AddPrimitiveCommand(p));
+
+        r.make_fragments(commands);
+        r.render_fragments(f);
+
+        std::vector<Color> expected(100);
+        expected.at(10) = red;
+        expected.at(20) = red;
+        expected.at(30) = red;
+        expected.at(40) = red;
+        expected.at(50) = red;
+        expected.at(60) = red;
+        expected.at(70) = red;
+        expected.at(80) = red;
+        expected.at(90) = red;
+
+
+        for (int i = 0; i < 100; i++) {
+            INFO("Check failed at ", i, " Expected: ", expected.at(i).to_hex(), " Found: ", f.get_frame_buffer().at(i).to_hex());
+            CHECK(f.get_frame_buffer().at(i) == expected.at(i));
+        }
+    }
+
+    SUBCASE("Vertical line with top and bottom clipping") {
+        Primitive p(PRIMITIVE_TYPE::LINE, Vertex(0, -1), Vertex(0, 30));
+        p.color = Color(255, 0, 0);
+
+
+        commands.push_back(AddPrimitiveCommand(p));
+
+        r.make_fragments(commands);
+        r.render_fragments(f);
+
+        std::vector<Color> expected(100);
+        expected.at(0) = red;
+        expected.at(10) = red;
+        expected.at(20) = red;
+        expected.at(30) = red;
+        expected.at(40) = red;
+        expected.at(50) = red;
+        expected.at(60) = red;
+        expected.at(70) = red;
+        expected.at(80) = red;
+        expected.at(90) = red;
+
+
+        for (int i = 0; i < 100; i++) {
+            INFO("Check failed at index ", i, " Expected: ", expected.at(i).to_hex(), " Found: ", f.get_frame_buffer().at(i).to_hex());
+            CHECK(f.get_frame_buffer().at(i) == expected.at(i));
+        }
+    }
+
     SUBCASE("Horizontal line with left clipping") {
         Primitive p(PRIMITIVE_TYPE::LINE, Vertex(-5, 0), Vertex(5, 0));
         p.color = Color(255, 0, 0);
